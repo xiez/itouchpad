3d8f960b81a002c0d48c41dffc00d02ab021db9c
willdtz <willdtz@1a0c5f6d-7b46-0410-8fe3-e1501f2db489>
2008-02-19 23:25:55 +0000

    Add ideas list...
    removed debug prints from itp-server

--- linux/itp-server.c
+++ linux/itp-server.c
@@ -136,12 +136,12 @@ int main( int argc, char ** argv)
 						XTestFakeRelativeMotionEvent( dpy, pEvent->move_info.dx, pEvent->move_info.dy, 0 );
 						break;
 					case EVENT_TYPE_MOUSE_DOWN:
-						printf( "mouse down: %d", pEvent->button_info.button );
+						//printf( "mouse down: %d", pEvent->button_info.button );
 						XTestFakeButtonEvent( dpy, pEvent->button_info.button, 1, 0 );
 						break;
 
 					case EVENT_TYPE_MOUSE_UP:	
-						printf( "mouse up: %d", pEvent->button_info.button );
+						//printf( "mouse up: %d", pEvent->button_info.button );
 						XTestFakeButtonEvent( dpy, pEvent->button_info.button, 0, 0 );
 						break;
 
